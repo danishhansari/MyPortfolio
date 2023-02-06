e7a276a70651845b893ad2cc0a35a6174e484607
Danish <dan71ish@gmail.com>
2023-02-06 10:54:06 +0530

    React section

--- script.js
+++ script.js
@@ -48,20 +48,19 @@ filtersEl.addEventListener('click', e => {
     projectEl5.style.transform = 'scale(1)';
     projectEl6.style.opacity = '1';
     projectEl6.style.transform = 'scale(1)';
-  } else if (filterText === 'Front end') {
-    projectEl1.style.opacity = '0';
-    projectEl1.style.transform = 'scale(0)';
-    projectEl2.style.opacity = '0';
-    projectEl2.style.transform = 'scale(0)';
+  } else if (filterText === 'React') {
+    projectEl1.style.opacity = '1';
+    projectEl1.style.transform = 'scale(1)';
+    projectEl3.style.opacity = '0';
+    projectEl3.style.transform = 'scale(0)';
     projectEl5.style.opacity = '0';
     projectEl5.style.transform = 'scale(0)';
     projectEl6.style.opacity = '0';
     projectEl6.style.transform = 'scale(0)';
-
-    projectEl3.style.transform = 'translateX(-650px)';
-    projectEl3.style.opacity = '1';
-    projectEl4.style.transform = 'translate(325px, -502px)';
-    projectEl4.style.opacity = '1';
+    projectEl2.style.transform = 'translate(0px, 0px)';
+    projectEl2.style.opacity = '1';
+    projectEl4.style.transform = 'scale(0)';
+    projectEl4.style.opacity = '0';
   } else if (filterText === 'Back end') {
     projectEl1.style.opacity = '0';
     projectEl1.style.transform = 'scale(0)';
@@ -71,24 +70,8 @@ filtersEl.addEventListener('click', e => {
     projectEl3.style.transform = 'scale(0)';
     projectEl4.style.opacity = '0';
     projectEl4.style.transform = 'scale(0)';
-
     projectEl5.style.transform = 'translate(-325px, -359px)';
     projectEl5.style.opacity = '1';
     projectEl6.style.transform = 'translate(-325px, -501px)';
     projectEl6.style.opacity = '1';
-  } else if (filterText === 'Full stack') {
-    projectEl3.style.opacity = '0';
-    projectEl3.style.transform = 'scale(0)';
-    projectEl4.style.opacity = '0';
-    projectEl4.style.transform = 'scale(0)';
-    projectEl5.style.opacity = '0';
-    projectEl5.style.transform = 'scale(0)';
-    projectEl6.style.opacity = '0';
-    projectEl6.style.transform = 'scale(0)';
-
-    projectEl1.style.opacity = '1';
-    projectEl1.style.transform = 'scale(1)';
-    projectEl2.style.opacity = '1';
-    projectEl2.style.transform = 'scale(1)';
-  }
-});
+  }});
